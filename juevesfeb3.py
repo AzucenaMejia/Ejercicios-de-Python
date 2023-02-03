26fea88e8b4bb939c8e2b7549410c8928e65288f
AzucenaMejia <blazu.mejia11@gmail.com>
2023-02-03 12:51:45 -0500

    mgs:se realiza un ejercicio de python

--- juevesfeb3.py
+++ juevesfeb3.py
@@ -10,4 +10,7 @@ print ("la suma de a+b es : ", c)
 mensaje =True
 print (f"la suma de {a}+{b} es igual a {c} ")
 
-#
+#Escribir un programa que lea un entero positivo, , introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta . La suma de los primeros enteros positivos puede ser calculada de la siguiente forma:  suma= n(n+1)/2
+n=int(input("ingresa un entero positivo: "))
+suma=n*(n+1)/2
+print("La suma de los primeros números enteros desde 1 hasta " + str(n) + " es " + str(suma))
